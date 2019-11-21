8483d4caaa6f8d1f7135d40a65d9dec55d095f19
DitlevR <ditlev1997@hotmail.com>
2019-11-21 17:43:41 +0100

    rest api connection

--- src/App.js
+++ src/App.js
@@ -1,62 +1,81 @@
-
-import React, { Component } from 'react';
-
+import "bootstrap/dist/css/bootstrap.css";
+import React, { Component } from "react";
+const url = "https://idon.dk/semesterprojekt/";
 class App extends Component {
-    state = {
-        query: '',
-        data: [],
-    }
+  state = {
+    query: "",
+    data: []
+  };
 
-    handleInputChange = () => {
+  handleInputChange = () => {
+    this.setState({
+      query: this.search.value
+    });
+    this.filterArray();
+  };
+
+  getData = () => {
+    fetch(url + "api/book/allbooks")
+      .then(response => response.json())
+      .then(responseData => {
+        console.log(responseData);
         this.setState({
-            query: this.search.value
-        })
-        this.filterArray();
-    }
+          data: responseData
+        });
+        console.log("state", this.state.data);
+      });
+  };
 
-    getData = () => {
-        fetch(`url`)
-        .then(response => response.json())
-        .then(responseData => {
-            // console.log(responseData)
-            this.setState({
-                data:responseData
-            })
-        })
+  filterArray = () => {
+    var searchString = this.state.query;
+    var responseData = this.state.data;
+    if (searchString.length > 0) {
+      // console.log(responseData[i].name);
+      responseData = responseData.filter(l => {
+        console.log(l.name.toLowerCase().match(searchString));
+      });
     }
+  };
 
-    filterArray = () => {
-        var searchString = this.state.query;
-        var responseData = this.state.data
-        if(searchString.length > 0){
-            // console.log(responseData[i].name);
-            responseData = responseData.filter(l => {
-                console.log( l.name.toLowerCase().match(searchString));
-            })
-        }
-    }
+  componentWillMount() {
+    this.getData();
+  }
+  render() {
+    return (
+      <div className="searchForm">
+        <form>
+          <input
+            type="text"
+            id="filter"
+            placeholder="Search for..."
+            ref={input => (this.search = input)}
+            onChange={this.handleInputChange}
+          />
+        </form>
+        <div>
+          <table>
+            <thead>
+              <th>Titel</th>
+              <th>Beskrivelse</th>
+              <th>Antal sider</th>
+              <th>Forfatter</th>
+            </thead>
 
-    componentWillMount() {
-        this.getData();
-    }
-    render() {
-        return (
-            <div className="searchForm">
-                <form>
-                    <input type="text" id="filter" placeholder="Search for..." ref={input => this.search = input} onChange={this.handleInputChange}/>
-                </form>
-                <div>
-                    {
-                        this.state.data.map((i) =>
-                            <p>{i.name}</p>
-                        )
-                    }
+            {this.state.data.map((book, i) => {
+              return (
+                <div key={i}>
+                  <tr>{book.title}</tr>
+                  <tr>{book.description}</tr>
+                  <tr>{book.pageNumber}</tr>
+                  <tr>{book.author}</tr>
                 </div>
-            </div>
-        )
-    }
+              );
+            })}
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
